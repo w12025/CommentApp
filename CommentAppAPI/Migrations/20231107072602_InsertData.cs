47e7f7d458616679f0716b1762174aff1059508b
w12025 <john0xsd@gmail.com>
2023-11-07 12:33:10 +0500

    Add insert data migrations

---
+++ CommentAppAPI/Migrations/20231107072602_InsertData.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CommentAppAPI.Migrations
+{
+    public partial class InsertData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sqlFile = Path.Combine(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName, @"insert-data.sql"); 
+            migrationBuilder.Sql(File.ReadAllText(sqlFile));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
