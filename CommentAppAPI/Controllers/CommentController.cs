fc49ab545974b0097a20f0ce32d9029d855d03f0
w12025 <john0xsd@gmail.com>
2023-11-07 11:43:22 +0500

    Add comments to CommentController

--- CommentAppAPI/Controllers/CommentController.cs
+++ CommentAppAPI/Controllers/CommentController.cs
@@ -15,38 +15,51 @@ public class CommentController : ControllerBase
         _commentRepository = commentRepository;
     }
 
+// GET request to retrieve all comments
     [HttpGet]
     public IEnumerable<Comment> GetAll()
     {
+        // Retrieve and return all comments from the repository
         return _commentRepository.GetComments();
     }
 
+// GET request to retrieve a comment by its ID
     [HttpGet]
     [Route("{id}")]
     public Comment Get(int id)
     {
+        // Retrieve and return a comment by its ID from the repository
         return _commentRepository.GetCommentById(id);
     }
 
+// POST request to insert a new comment
     [HttpPost]
     public string Insert(Comment comment)
     {
+        // Insert the provided comment into the repository and get the assigned ID
         int id = _commentRepository.InsertComment(comment);
+        // Return a message indicating that the comment was added with its new ID
         return "ADDED WITH ID = " + id;
     }
 
+// PUT request to update an existing comment
     [HttpPut]
     public string Update(Comment comment)
     {
+        // Update the provided comment in the repository
         _commentRepository.UpdateComment(comment);
+        // Return a message indicating that the comment was updated
         return "UPDATED";
     }
 
+// DELETE request to delete a comment by its ID
     [HttpDelete]
     [Route("{id}")]
-    public String Delete(int id)
+    public string Delete(int id)
     {
+        // Delete a comment by its ID from the repository
         _commentRepository.DeleteComment(id);
+        // Return a message indicating that the comment was deleted
         return "DELETED";
     }
 }
