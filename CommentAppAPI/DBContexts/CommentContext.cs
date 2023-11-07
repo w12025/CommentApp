3164da7cbf2b97446869454138c0cd94401a8efc
w12025 <akmaljonsamandarov@gmail.com>
2023-11-07 09:54:28 +0500

    Configure db connection string

---
+++ CommentAppAPI/DBContexts/CommentContext.cs
@@ -0,0 +1,10 @@
+using CommentAppAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommentAppAPI.DBContexts;
+
+public class CommentContext: DbContext
+{
+    public CommentContext(DbContextOptions<CommentContext> options) : base(options){}
+    public DbSet<Comment> Comments { get; set; }
+}
--- CommentAppAPI/Program.cs
+++ CommentAppAPI/Program.cs
@@ -1,11 +1,29 @@
+using CommentAppAPI.DBContexts;
+using CommentAppAPI.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<CommentContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CommentDb")));
 
+// Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddTransient<ICommentRepository, CommentRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(corsPolicyBuilder =>
+    {
+        corsPolicyBuilder.WithOrigins("https://localhost:5002") // URL of the frontend
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -16,8 +34,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseHttpsRedirection();
+
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors();
 
 app.Run();
