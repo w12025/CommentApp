8cc5ce975466cc5de18499213e4ffdbc7ab4dec8
w12025 <149996966+w12025@users.noreply.github.com>
2023-11-07 20:39:40 +0500

    Delete CommentAppWeb directory

--- CommentAppWeb/Models/Comment.cs
+++
@@ -1,9 +0,0 @@
-namespace CommentAppWeb.Models;
-
-public class Comment
-{
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public string? Description { get; set; }
-    public string Username { get; set; }
-}
